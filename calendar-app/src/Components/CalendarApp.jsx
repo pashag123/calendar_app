4d87bd41823cf7ae3fa12aeed6b1579772c4d899
pashag123 <pashag123@gmail.com>
2024-09-04 14:02:06 -0700

    were getting started

---
+++ calendar-app/src/Components/CalendarApp.jsx
@@ -0,0 +1,19 @@
+const CalendarApp = () => {
+    return (
+        <div className="calendar-app">
+            <div className="calendar">
+                <h1 className="heading">Calendar</h1>
+                <div className="navigate-date">
+                    <h2 className="month">May,</h2>
+                    <h2 className="year">2024</h2>
+                    <div className="buttons">
+                        <i className="bx bx-chevron-left"></i>
+                        <i className="bx bx-chevron-right"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default CalendarApp
